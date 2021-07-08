8c24558a907cd3613550e5c440be885ed487693e
BlackCcccCcc <827187153@qq.com>
2021-07-08 17:24:46 +0800

    本地提交

--- a.js
+++ a.js
@@ -1,2 +1,18 @@
+"use strict";
+exports.__esModule = true;
 var helloWorld;
-console.log(helloWorld);
+var a;
+var b;
+var someWhere = {
+    Plant: '蓝色星球',
+    country: '核泽之乡',
+    provice: '东京都',
+    street: '下北泽 '
+};
+var eva00 = {
+    event: "下北泽红茶事件",
+    date: new Date,
+    roles: ['24岁的学生', '红茶师', '健身教练'],
+    location: someWhere
+};
+console.log(eva00);
--- a.ts
+++ a.ts
@@ -1,5 +1,19 @@
+import { Evet, Place } from "./types";
+
 let helloWorld: string;
 let a:number;
 let b:string;
-console.log(helloWorld);
+let someWhere: Place ={
+    Plant: '蓝色星球',
+    country: '核泽之乡',
+    provice: '东京都',
+    street: '下北泽 '
+}
+let eva00:Evet = {
+    event:  "下北泽红茶事件",
+    date: new Date,
+    roles: ['24岁的学生','红茶师','健身教练'],
+    location: someWhere
+}
+console.log(eva00);
 
